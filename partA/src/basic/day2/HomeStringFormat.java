4ecc045f4a6d588b24373fa2bbc7b6e13ff55269
leedaehwan <eoghks410@naver.com>
2023-12-29 17:31:51 +0900

    day2

---
+++ partA/src/basic/day2/HomeStringFormat.java
@@ -0,0 +1,14 @@
+package basic.day2;
+
+public class HomeStringFormat {
+    
+    public static void main(String[] args) {
+        //String.format()에 대한 예제를 작성하고 실행도 해보세요.
+        
+
+
+
+
+
+    }
+}
